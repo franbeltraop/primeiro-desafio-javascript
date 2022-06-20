97e245aed1d70492a59f7658c82b5820d841b747
franbeltraop <franbeltraop@outlook.com>
2022-06-20 16:06:24 -0300

    adição de função if

---
+++ assets/js/scripts;.js
@@ -0,0 +1,25 @@
+var currentNumberWrapper = document.getElementById ("currentNumber")
+var currentNumber = 0
+
+function increment () {
+ if (currentNumber <10) {
+    currentNumber = currentNumber + 1 } 
+    else {
+      currentNumber = currentNumber
+    }
+  
+    currentNumberWrapper.innerHTML = currentNumber;
+}
+
+function decrement () {
+ if (currentNumber > -10) {
+      currentNumber = currentNumber - 1 } 
+      else {
+        currentNumber = currentNumber
+      }
+        if (currentNumber <0) {
+        currentNumberWrapper.style.color = "red";}
+      
+      currentNumberWrapper.innerHTML = currentNumber;
+    
+}
